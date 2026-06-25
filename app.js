de4d6c25d41cf522399583eee894ed2af44808e3
rasoulnasserifreelancer <rasoulnasseri_freelancer@yahoo.com>
2026-06-25 15:04:12 +0330

    added the main structure of the server

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-console.log("testing");
-console.log("testing");
+const app = require('express'); // create an express application
+
+// different routes here 
+
+
+app.listen(3000); // server is running on port 3000 on dev mode
