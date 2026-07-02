1ffc314459d2f8b33cdd616731370fe29227d1e2
rasoulnasserifreelancer <rasoulnasseri_freelancer@yahoo.com>
2026-07-02 12:23:30 +0330

    i modified error handling behaviour

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -7,104 +7,76 @@ const {
 } = require("../database/database");
 const router = express.Router();
 
-router.get(
-  "/",
-  async (req, res, next) => {
-    try {
-      const allPosts = await loadData(
-        "SELECT p.*, a.full_name AS fullName FROM posts p INNER JOIN authors a ON p.author_id=a.id",
-      );
-      console.log(allPosts);
-      res.render("posts", { allPosts });
-    } catch (error) {
-      next(error);
-    }
-  },
-  (error, req, res, next) => {
-    if (error) {
-      console.log(error);
-      res.render("500");
-    }
-  },
-);
+router.get("/", async (req, res, next) => {
+  try {
+    const allPosts = await loadData(
+      "SELECT p.*, a.full_name AS fullName FROM posts p INNER JOIN authors a ON p.author_id=a.id",
+    );
+    console.log(allPosts);
+    res.render("posts", { allPosts });
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get("/view/:id", async (req, res, next) => {
-  const thePost = await loadData(
-    "SELECT p.*, a.full_name, a.email AS fullName FROM posts p INNER JOIN authors a ON p.author_id=a.id WHERE p.id = ?",
-    [req.params.id],
-  );
-  if (!thePost || thePost.length === 0) return res.status(404).render("404");
-  res.render("postDetail", { thePost: thePost[0] });
+  try {
+    const thePost = await loadData(
+      "SELECT p.*, a.full_name, a.email AS fullName FROM posts p INNER JOIN authors a ON p.author_id=a.id WHERE p.id = ?",
+      [req.params.id],
+    );
+    if (!thePost || thePost.length === 0) return res.status(404).render("404");
+    res.render("postDetail", { thePost: thePost[0] });
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get(
-  "/edit/:id",
-  async (req, res, next) => {
-    try {
-      const [post] = await loadData("SELECT * FROM posts WHERE id=?", [
-        req.params.id,
-      ]);
-      if (!post) return res.render("404");
-      res.render("edit-post", { post });
-    } catch (error) {
-      if (error) next(error);
-    }
-  },
-  (e, req, res, next) => {
-    if (e) {
-      console.log(e);
-      res.render("500");
-    }
-  },
-);
-
-router.post(
-  "/edit/:id",
-  async (req, res, next) => {
-    let values = [];
-    values = values.concat(Object.values(req.body)).concat(+req.params.id);
-    console.log("values", values);
-    try {
-      const result = await updatePost(values);
-      res.redirect("/");
-    } catch (error) {
-      next(error);
-    }
-  },
-  (e, req, res, next) => {
-    if (e) {
-      console.log(e, "here");
-      res.render("500");
-    }
-  },
-);
-
-router.get("/create", async (req, res) => {
-  const sql = "SELECT * FROM authors";
-  const authors = await loadData(sql);
-  res.render("create-post", { authors });
+router.get("/edit/:id", async (req, res, next) => {
+  try {
+    const [post] = await loadData("SELECT * FROM posts WHERE id=?", [
+      req.params.id,
+    ]);
+    if (!post) return res.render("404");
+    res.render("edit-post", { post });
+  } catch (error) {
+    if (error) next(error);
+  }
 });
 
-router.post(
-  "/create",
-  async (req, res, next) => {
-    const values = { ...req.body, author_id: +req.body.author_id };
-    try {
-      console.log(values);
-      const resp = await savePost(Object.values(values));
-      console.log(resp);
-      res.redirect("/");
-    } catch (e) {
-      next(e);
-    }
-  },
-  (e, req, res, next) => {
-    if (e) {
-      console.log(e);
-      res.render("500");
-    }
-  },
-);
+router.post("/edit/:id", async (req, res, next) => {
+  let values = [];
+  values = values.concat(Object.values(req.body)).concat(+req.params.id);
+  console.log("values", values);
+  try {
+    const result = await updatePost(values);
+    res.redirect("/");
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/create", async (req, res, next) => {
+  try {
+    const sql = "SELECT * FROM authors";
+    const authors = await loadData(sql);
+    res.render("create-post", { authors });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/create", async (req, res, next) => {
+  const values = { ...req.body, author_id: +req.body.author_id };
+  try {
+    console.log(values);
+    const resp = await savePost(Object.values(values));
+    console.log(resp);
+    res.redirect("/");
+  } catch (e) {
+    next(e);
+  }
+});
 
 router.post(
   "/delete",
