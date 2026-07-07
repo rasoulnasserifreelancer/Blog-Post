50528c7caf52c83d57d106b0d5507eefc7475f66
rasoulnasserifreelancer <rasoulnasseri_freelancer@yahoo.com>
2026-07-07 12:48:58 +0330

    i added the compression package to compress http response sent to the user

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const path = require('path'); 
 const express = require('express'); 
+const compression = require('compression');
 const debug = require('debug');
 const app = express(); // create an express application
 debug('express'); // adding express-logger
 const log = debug('start:server'); // adding start server logger
 const errorLog = debug('error:server'); // adding start server logger
 const dotevn = require('dotenv');
-
-
-
 dotevn.config();
+
 const {createPool} = require('./database/database');
 const defaultRouter = require('./routes/defaultRoutes');
 const postRouter = require('./routes/postRoutes');
@@ -22,7 +21,7 @@ app.set('views', path.join(__dirname, 'views')); //addressing my templates
 //using middlewares for incoming requests
 app.use(express.urlencoded({extended:false}) ); // decoding urlencode HTTP request
 app.use('/static',express.static('public')); //serving static files from public folder and with virtual path prefix named static
-
+app.use(compression());
 // adding route handlers
 app.use('/', defaultRouter);
 app.use('/posts', postRouter); 
