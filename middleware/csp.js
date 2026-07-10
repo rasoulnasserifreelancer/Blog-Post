218a42c0f862b78e090885a0c85f84851a629052
rasoulnasserifreelancer <rasoulnasseri_freelancer@yahoo.com>
2026-07-10 13:43:47 +0330

    my middlewares are added

---
+++ middleware/csp.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const crypto = require("crypto");
+const router = express.Router();
+
+/*
+
+this will set nonce for document and also
+for each style but nonce generated for document
+will be checked by the browser and that nonce is exactly 
+what is passed to head tag, browser kind of ignore
+nonce generated for styles
+
+*/
+
+
+router.use((req, res, next) => {
+  const nonce = crypto.randomUUID();
+  res.locals.nonce = nonce;
+  res.setHeader(
+    "Content-Security-Policy",
+    `default-src 'self'; script-src 'nonce-${nonce}' ; style-src 'nonce-${nonce}'`,
+  );
+  console.log('nonce', nonce, req.path);
+  next();
+});
+
+module.exports = router;
