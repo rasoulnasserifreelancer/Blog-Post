1707c96ba89fa674bcd5ca6e4d0cf055f4c74e10
rasoulnasserifreelancer <rasoulnasseri_freelancer@yahoo.com>
2026-07-12 07:24:56 +0330

    completeing ratelimit middleware

--- middleware/rateLimit.js
+++ middleware/rateLimit.js
@@ -1,1 +1,18 @@
+const express = require('express');
 const rateLimit = require('express-rate-limit');
+const router = express.Router();
+
+
+
+// configuring ratelimiter to put limit of 15 request per minute
+const limiter = rateLimit({
+    max:15,
+    windowMS:1000*60
+})
+
+
+// applying limiter to all incoming requests to my routes
+router.use(limiter);
+
+
+module.exports = router;
