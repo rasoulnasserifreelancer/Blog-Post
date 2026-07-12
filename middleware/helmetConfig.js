8f638dba6b3ad985f83ae052f5dc6fb48eebbb9d
rasoulnasserifreelancer <rasoulnasseri_freelancer@yahoo.com>
2026-07-12 07:19:09 +0330

    adding middleware folder with two files

---
+++ middleware/helmetConfig.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const crypto = require("crypto");
+const helmet = require('helmet')
+const router = express.Router();
+
+/*
+
+this will set nonce for document and also
+for each style but nonce generated for document
+will be checked by the browser and that nonce is exactly 
+what is passed to head tag, browser kind of ignore
+nonce generated for styles
+
+*/
+
+
+router.use((req, res, next) => {
+  const nonce = crypto.randomUUID();
+  res.locals.nonce = nonce;
+  // res.setHeader(
+  //   "Content-Security-Policy",
+  //   `default-src 'self'; script-src 'nonce-${nonce}' ; style-src 'nonce-${nonce}'`,
+  // );
+  // console.log('nonce', nonce, req.path);
+  next();
+});
+
+router.use(helmet({
+  directives : {
+    contentSecurityPolicy : {
+      'script-src' : ["'self'", (req, res) => `'nonce-${res.locals.nonce}'` ]
+    }
+  }
+}))
+
+module.exports = router;
---
+++ middleware/rateLimit.js
@@ -0,0 +1,1 @@
+const rateLimit = require('express-rate-limit');
