1edf8ff28c7d3c92ee703d73b627b9ca02fd81f8
rasoulnasserifreelancer <rasoulnasseri_freelancer@yahoo.com>
2026-07-16 18:26:40 +0330

    modified .env file

--- database/db.js
+++ database/db.js
@@ -4,17 +4,17 @@ const mysql2 = require("mysql2/promise");
 let pool;
 try {
   console.log({
-  host: process.env["DB-HOST"],
-  port: process.env["DB-PORT"],
-  user: process.env["DB-USER"],
-  database: process.env["DB-DATABASE"],
+  host: process.env["DB_HOST"],
+  port: process.env["DB_PORT"],
+  user: process.env["DB_USER"],
+  database: process.env["DB_DATABASE"],
 });
   pool = mysql2.createPool({
-    user: process.env["DB-USER"],
-    password: process.env["DB-PASSWORD"],
-    port : process.env["DB-PORT"],
-    host: process.env["DB-HOST"],
-    database: process.env["DB-DATABASE"],
+    user: process.env["DB_USER"],
+    password: process.env["DB_PASSWORD"],
+    port : process.env["DB_PORT"],
+    host: process.env["DB_HOST"],
+    database: process.env["DB_DATABASE"],
     ssl: {
       ca: fs.readFileSync("./isrgrootx1.pem"), // Certificate Authority (CA) for TLS connection
       rejectUnauthorized: true,
